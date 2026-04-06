2123cc3a917af4f970e37c1c84614b120b02b6e6
danprsp <grapo747@gmail.com>
2026-04-05 23:01:34 -0300

    fix(not-us) Fix Permissao de Rotas do Front

--- src/main/java/com/kernelpanic/apontamentohoras_service/ApontamentoHorasApplication.java
+++ src/main/java/com/kernelpanic/apontamentohoras_service/ApontamentoHorasApplication.java
@@ -12,16 +12,16 @@ public class ApontamentoHorasApplication {
 		SpringApplication.run(ApontamentoHorasApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
-						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-						.allowedHeaders("*");
-			}
-		};
-	}
+	// @Bean
+	// public WebMvcConfigurer corsConfigurer() {
+	// 	return new WebMvcConfigurer() {
+	// 		@Override
+	// 		public void addCorsMappings(CorsRegistry registry) {
+	// 			registry.addMapping("/**")
+	// 					.allowedOrigins("http://localhost:3000")
+	// 					.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+	// 					.allowedHeaders("*");
+	// 		}
+	// 	};
+	// }
 }
