158fcd8d7794500bffa517192234cbef9c92e885
Henry Tito <140865436+HenryTito@users.noreply.github.com>
2026-05-31 20:48:22 -0300

    git commit -m "fix(US61): corrigir conversao de datas no filtro de horas"

--- src/main/java/com/kernelpanic/apontamentohoras_service/dtos/HorasFiltroDTO.java
+++ src/main/java/com/kernelpanic/apontamentohoras_service/dtos/HorasFiltroDTO.java
@@ -3,6 +3,7 @@ package com.kernelpanic.apontamentohoras_service.dtos;
 import java.time.LocalDate;
 import com.kernelpanic.apontamentohoras_service.enums.EstadoHora;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 public class HorasFiltroDTO {
@@ -10,6 +11,8 @@ public class HorasFiltroDTO {
     private Long projetoId;
     private Long tarefaId;
     private EstadoHora estado;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate dataInicio;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate dataFim;
 }
